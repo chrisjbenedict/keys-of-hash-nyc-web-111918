a64cb4db8329512e2c7147341d19053f090eb029
Christopher Benedict <chrisjbenedict@gmail.com>
2018-11-05 16:31:18 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,9 +2,7 @@ class Hash
   def keys_of(*arguments)
     # code goes here
     #compact returns a hash with non nil values.
-    arguments.map do |key, value| 
-      puts arguments.include?(value) ? key : nil
-    end.compact
+    map {|key, value| arguments.include?(value) ? key : nil }.compact
 
   end
 end
